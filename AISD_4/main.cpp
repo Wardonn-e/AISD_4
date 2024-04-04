5ecaa6056b8cd08034ff78e9733ac5f157d65c82
wardonne <Wardonne@yandex.ru>
2024-04-04 21:42:38 +0400

    Initial Commit

---
+++ AISD_4/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  AISD_4
+//
+//  Created by Роман Казаев on 04.04.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
